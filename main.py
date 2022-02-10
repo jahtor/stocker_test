55fa5f8bef8c3f2edb0ca5175ab600e97729e411
jahtor <kinesis@mail.ru>
2022-02-10 14:16:13 +0300

    10

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
-import json
+import os
+import pandas as pd
+import secrets
+from datetime import datetime
 from stock_data import StockData
+import matplotlib.pyplot as plt
 
-StockData('FB').download_to_numpy()
-# d = StockData('FB')._sec
-# print('Info')
-# print(json.dumps(d.info, indent=4, sort_keys=True))
+
+def train_LSTM_network(stock, start_date, validation_date, project_folder):
+    data = StockData(stock, start_date, validation_date, project_folder)
+    (x_train, y_train), (x_test, y_test), (training_data, test_data) = data.download_to_numpy(TIME_STEPS)
+
+    # print("plotting Data and Histogram")
+    plt.figure(figsize=(12, 5))
+    plt.plot(training_data.Close, color='green')
+    plt.plot(test_data.Close, color='red')
+    plt.ylabel('Price [' + data.get_currency() + ']')
+    plt.xlabel("Date")
+    plt.legend(["Training Data", "Validation Data >= " + validation_date.strftime("%Y-%m-%d")])
+    plt.title(data.get_short_name())
+    # plt.savefig(os.path.join(data.project_folder, data.get_short_name().strip().replace('.', '') + '_price.png'))
+    # fig, ax = plt.subplots()
+    # training_data.hist(ax=ax)
+    # fig.savefig(os.path.join(data.project_folder, data.get_short_name().strip().replace('.', '') + '_hist.png'))
+    plt.pause(0.001)
+    plt.show()
+
+
+if __name__ == '__main__':
+    STOCK_TICKER = 'BTC-USD'
+    STOCK_START_DATE = pd.to_datetime('2013-04-28')
+    STOCK_VALIDATION_DATE = pd.to_datetime('2021-01-01')
+    EPOCHS = 100
+    BATCH_SIZE = 32
+    TIME_STEPS = 3
+    TODAY_RUN = datetime.today().strftime("%Y%m%d")
+    # TOKEN = STOCK_TICKER + '_' + TODAY_RUN + '_' + secrets.token_hex(16)
+    TOKEN = STOCK_TICKER + '_' + TODAY_RUN
+
+    print('Ticker: ' + STOCK_TICKER)
+    print('Start date: ' + STOCK_START_DATE.strftime("%Y-%m-%d"))
+    print('Validation date: ' + STOCK_VALIDATION_DATE.strftime("%Y-%m-%d"))
+    print('Test Run Folder: ' + TOKEN)
+
+    PROJECT_FOLDER = os.path.join(os.getcwd(), 'data', TOKEN)
+    if not os.path.exists(PROJECT_FOLDER):
+        os.makedirs(PROJECT_FOLDER)
+
+    train_LSTM_network(STOCK_TICKER, STOCK_START_DATE, STOCK_VALIDATION_DATE, PROJECT_FOLDER)
--- stock_data.py
+++ stock_data.py
@@ -1,16 +1,22 @@
 import os
+import numpy as np
+import pandas as pd
 import yfinance as yf
 import datetime
-# from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
+
 
 class StockData:
-    def __init__(self, stock):
+    def __init__(self, stock, start_date, validation_date, project_folder):
         self._stock = stock
-        # self._sec = yf.Ticker(self._stock.get_ticker())
         self._sec = yf.Ticker(self._stock)
-        # self._min_max = MinMaxScaler(feature_range=(0, 1))
+        self.start_date = start_date
+        self.validation_date = validation_date
+        self.project_folder = project_folder
+        self._min_max = MinMaxScaler(feature_range=(0, 1))
 
     def __data_verification(self, train):
+        print('Data Verification')
         print('Средняя:', train.mean(axis=0))
         print('Максимум:', train.max())
         print('Минимум:', train.min())
@@ -25,34 +31,48 @@ class StockData:
     def get_ticker(self):
         return self._sec.info['symbol']
 
-    def get_start_date(self):
-        return self._sec.info['startDate']
-
-    # def download_to_numpy(self, time_steps, project_folder):
-    def download_to_numpy(self):
+    def download_to_numpy(self, time_steps):
         end_date = datetime.date.today()
         # print('End Date: ' + end_date.strftime("%Y-%m-%d"))
-        data = yf.download(tickers=[self._stock], start=self.get_start_date(), end=end_date)[['Close']]
+        data = yf.download(tickers=[self._stock], start=self.start_date, end=end_date)[['Close']]
         data = data.reset_index()
-        datafile = self._stock + '_downloaded_data' + '.csv'
-        # data.to_csv(os.path.join('./data', datafile))
-        # print(data)
+        data.to_csv(os.path.join(self.project_folder, self._stock + '_downloaded_data' + '.csv'))
 
-        training_data = data[data['Date'] < '2020-01-01'].copy()
-        test_data = data[data['Date'] >= '2020-01-01'].copy()
+        # разделяем данные на training & test
+        training_data = data[data['Date'] < self.validation_date].copy()
+        test_data = data[data['Date'] >= self.validation_date].copy()
         # Set the data frame index using column Date
         training_data = training_data.set_index('Date')
+        # print(training_data)
         test_data = test_data.set_index('Date')
         # print(test_data)
 
-        # train_scaled = self._min_max.fit_transform(training_data)
-        # self.__data_verification(train_scaled)
-
-        self.__data_verification(training_data)
+        # усредняем результат (0-1)
+        train_scaled = self._min_max.fit_transform(training_data)
+        self.__data_verification(train_scaled)
 
         # Training Data Transformation
         x_train = []
         y_train = []
-        # for i in range(time_steps, training_data.shape[0]):
-        #     x_train.append(training_data[i - time_steps:i])
-        #     y_train.append(training_data[i, 0])
+        for i in range(time_steps, train_scaled.shape[0]):
+            x_train.append(train_scaled[i - time_steps:i])
+            y_train.append(train_scaled[i, 0])
+
+        x_train, y_train = np.array(x_train), np.array(y_train)
+        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+        total_data = pd.concat((training_data, test_data), axis=0)
+        inputs = total_data[len(total_data) - len(test_data) - time_steps:]
+        test_scaled = self._min_max.fit_transform(inputs)
+
+        # Testing Data Transformation
+        x_test = []
+        y_test = []
+        for i in range(time_steps, test_scaled.shape[0]):
+            x_test.append(test_scaled[i - time_steps:i])
+            y_test.append(test_scaled[i, 0])
+
+        x_test, y_test = np.array(x_test), np.array(y_test)
+        x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+        return (x_train, y_train), (x_test, y_test), (training_data, test_data)
